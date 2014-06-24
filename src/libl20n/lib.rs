f95590cc8383f8cae5de46c6fabe4bf1eed30e31
Sean McArthur <sean.monstar@gmail.com>
2014-06-24 14:25:51 -0700

    warn mising docs

--- src/libl20n/lib.rs
+++ src/libl20n/lib.rs
@@ -2,13 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+//! L20n implementation for localization
+//!
+//! This crate provides an interface to use l20n files to localize your
+//! application.
+
 #![crate_id = "l20n#0.1-pre"]
 #![license = "MPLv2"]
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
+#![deny(missing_doc)]
 
 extern crate serialize;
-
 
 pub mod compiler;
 pub mod data;
