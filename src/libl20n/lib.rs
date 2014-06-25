02027a974be1b3748c1559d2956eac3b357153f8
Sean McArthur <sean.monstar@gmail.com>
2014-06-24 19:32:00 -0700

    fixme for adding deny(warnings)

--- src/libl20n/lib.rs
+++ src/libl20n/lib.rs
@@ -44,6 +44,7 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://static.rust-lang.org/doc/master")]
+// FIXME: rust#14450: add deny(warnings)
 #![deny(missing_doc)]
 
 extern crate serialize;
