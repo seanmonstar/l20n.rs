a0377a9da514950158b64682a216c517cfc60b80
Andrew Paseltiner <apaseltiner@gmail.com>
2014-09-04 13:22:12 -0400

    remove deprecated use of `std::str::from_char`

--- src/data.rs
+++ src/data.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 use std::collections::HashMap;
-use std::str;
 
 use serialize;
 
@@ -89,7 +88,7 @@ impl<'a> serialize::Encoder<EncodeError> for Encoder {
     }
 
     fn emit_char(&mut self, v: char) -> EncoderResult {
-        self.data.push(Str(str::from_char(v)));
+        self.data.push(Str(v.to_string()));
         Ok(())
     }
 
